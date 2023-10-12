2f34f896dc5b5849c9e8c4c523d69451bfe2e2e5
Ioannis Maras <maras2002@gmail.com>
2023-10-12 22:59:30 +0300

    some small changes

--- lib/providers/websocket_notifier.dart
+++ lib/providers/websocket_notifier.dart
@@ -1,5 +1,6 @@
 // import 'dart:async';
 import 'dart:convert';
+import 'dart:ffi';
 import 'dart:io';
 import 'package:flutter/material.dart';
 // import 'package:provider/provider.dart';
@@ -8,8 +9,10 @@ class WebSocketNotifier extends ChangeNotifier {
   WebSocket? _webSocket;
   bool isConnected = false;
   String latestMessage = "No message received.";
+  late GameState gameState = GameState("lobby", [], null);
 
   Future<bool> connect(String url) async {
+    print(url);
     _webSocket = await WebSocket.connect(url);
     _webSocket!.listen(
       _handleMessage,
@@ -21,13 +24,36 @@ class WebSocketNotifier extends ChangeNotifier {
     // ... (other code)
 
     notifyListeners();
+
     await Future.delayed(const Duration(seconds: 2));
     return isConnected;
   }
 
   void _handleMessage(dynamic message) {
-    latestMessage = message.toString();
-    print(latestMessage);
+    Map<String, dynamic> jsonObject = jsonDecode(message.toString());
+    print(jsonObject);
+    print(jsonObject['type']);
+
+    if (jsonObject['type'] == 'player_change') {
+      gameState.players = [];
+      for (var player in jsonObject['all_players']) {
+        gameState.players.add(Player(player['channel_name'], player['username'],
+            player['role'], player['is_alive'], player['vote']));
+      }
+    }
+    // if (jsonObject is PlayerChangeEvent) {
+    //   print(jsonObject.type);
+    // } else {
+    //   print("not a player change event");
+    // }
+    // if (message.type == 'player_change') {
+    //   gameState.players = [];
+    //   // for (var player in message.all_players) {
+    //   //   gameState.players.add(Player(player.channel_name, player.username,
+    //   //       player.role, player.is_alive, player.vote));
+    //   // }
+    // }
+
     notifyListeners();
   }
 
@@ -59,4 +85,25 @@ class WebSocketNotifier extends ChangeNotifier {
     _webSocket?.close();
     super.dispose();
   }
+}
+
+class GameState {
+  String state;
+  List<Player> players;
+  // ignore: non_constant_identifier_names
+  String? story_teller;
+
+  GameState(this.state, this.players, this.story_teller);
+}
+
+class Player {
+  // ignore: non_constant_identifier_names
+  String channel_name;
+  String username;
+  String? role;
+  // ignore: non_constant_identifier_names
+  bool? is_alive;
+  Int? vote;
+
+  Player(this.channel_name, this.username, this.role, this.is_alive, this.vote);
 }
