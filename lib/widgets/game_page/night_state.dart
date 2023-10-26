6e74fd29cdd7aba239360822b230788e828a4e09
IoannisMaras <maras2002@gmail.com>
2023-10-26 19:29:23 +0300

    a little bit of progress

--- lib/widgets/game_page/night_state.dart
+++ lib/widgets/game_page/night_state.dart
@@ -223,6 +223,60 @@ class NightStep0 extends StatelessWidget {
   }
 }
 
+class NightStep3 extends StatelessWidget {
+  final GameState game;
+  const NightStep3({
+    required this.game,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      const Text(
+        'It is Nighttime',
+        style: TextStyle(fontSize: 30),
+      ),
+      //add an image here
+      const SizedBox(height: 20),
+      const Text(
+        'Please close your eyes',
+        style: TextStyle(fontSize: 20),
+      ),
+      const SizedBox(height: 20),
+      Container(
+        height: 300,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/night_state.jpg"),
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+        ),
+      ),
+      const Expanded(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Night falls on Palermo",
+            style: TextStyle(fontSize: 32),
+          ),
+          Text("Everyone has to close their eyes..."),
+        ],
+      )),
+      //button to go the next step
+      ElevatedButton(
+        onPressed: () {
+          Provider.of<NightStateStepperProvider>(context, listen: false)
+              .nextStep(game, null);
+        },
+        child: const Text('Next Step'),
+      ),
+    ]);
+  }
+}
+
 class NightStep1 extends StatelessWidget {
   final GameState game;
   const NightStep1({
